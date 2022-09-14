780030fef995b1a03dd9f4133a1fe54964fe1e22
Frknzdmr1 <ozdemirf97@icloud.com>
2022-09-13 23:54:19 -0400

    color is left

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
+
+var enteredNumber = document.querySelector('#entered-number');
+var numberRange = document.querySelector('#numberRange');
+var checkBtn = document.querySelector('#btn-check');
+var counts = document.querySelector('#counts');
+var guessedNumbers = document.querySelector('#guessed-numbers');
+var result = document.querySelector('#result');
+var newGameBtn = document.querySelector('.new-game');
+var currentScore = 0; 
 var randomNumber = Math.ceil(Math.random()*100);
-var enteredNumber = document.querySelector('.entered-number')
-var numberRange = document.querySelector('.numberRange')
-var checkBtn = document.querySelector('.btn-check')
-var counts = document.querySelector('.counts')
-var guessedNumbers = document.querySelector('.guessed-numbers')
-var result = document.querySelector('.result')
-var newGameBtn = document.querySelector('.new-game')
+checkBtn.addEventListener('click', function(){
 
+    var guessedNumber = document.querySelector('#entered-number').value;
 
-
-checkBtn.addEventListener('click' , function(){
-    var guessedNumber = document.querySelector('.entered-number').value;
-   
     if(guessedNumber < 0 || guessedNumber > 100){
-        alert('Please enter a number between 1 and 100')
-        guessedNumber.value = ''; // ya array de birak yada ='' yap
-        return
+    alert('Please enter a number between 1 and 100');
+       
     }
 
     if (randomNumber == guessedNumber){
         result.textContent = 'Congrats, your number is right!';
-    }else{
-        result.textContent = 'Wrong';
+        let backgroundColor = document.querySelector('#body')
+        
+    }else {
+        
     
     if (guessedNumber > randomNumber){
         result.textContent = 'Your number is too high';
 
     }else if(guessedNumber < randomNumber){
-        result.textContent = 'Your number is too low'
-    }
+        result.textContent = 'Your number is too low';
+    
+    }}currentScore++;
+    counts.textContent = currentScore
+    enteredNumber.value = ''
 
-    }
-})
+});
+
+newGameBtn.addEventListener('click', function(e){
+    result.textContent = 'Start guessing!'
+    counts.textContent = '0'
+    currentScore = '0'
+    counts.textContent = currentScore
+});
